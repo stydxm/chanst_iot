74248b0fcd8359dd6c23c65b3cd3c521b1badec7
stydxm <1773834430@qq.com>
2024-01-31 00:44:25 +0800

    修改手滑多写的字母

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ const Component = require("../models/Component")
 const logger = require("./logger")
 const time = require("./time")
 
-sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     dialect: "mysql",
@@ -26,7 +26,7 @@ module.exports.updateStatus = async (type, uuid) => {
             type: type + "_heartbeat",
             target: uuid
         },
-        order: ["creratedAt", "DESC"]
+        order: ["createdAt", "DESC"]
     })
     const timeDiff = Math.abs(new Date() - lastHeartbeatTime)
     if (timeDiff > 5000) {
